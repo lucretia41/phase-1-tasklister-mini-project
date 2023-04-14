52111760c61213f9862d47e5b1cbdc06fbe6adb9
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-14 12:54:41 -0600

    added monicapriority

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
   
-})
+// })
 
-const form = document.getElementById('create-task-form');
-form.addEventListener('submit', addToList);
-const uList = document.getElementById('tasks');
+// const form = document.getElementById('create-task-form');
+// form.addEventListener('submit', addToList);
+// const uList = document.getElementById('tasks');
 
-function addToList(eventObj) {
-  eventObj.preventDefault();
-  const listItem = document.createElement('li');
-  const newText = eventObj.target['new-task-description'].value;
-  listItem.textContent = newText;
-  uList.append(listItem);
-  eventObj.target.reset();
+// function addToList(eventObj) {
+//   eventObj.preventDefault();
+//   const listItem = document.createElement('li');
+//   const newText = eventObj.target['new-task-description'].value;
+//   listItem.textContent = newText;
+//   uList.append(listItem);
+//   eventObj.target.reset();
   
-  const btn = document.createElement('button');
-  btn.textContent = "REMOVE";
-  listItem.append(btn);
+//   const btn = document.createElement('button');
+//   btn.textContent = "REMOVE";
+//   listItem.append(btn);
 
-  btn.addEventListener('click', () => {
-    listItem.remove();
-  })
-//   add new functions here
+//   btn.addEventListener('click', () => {
+//     listItem.remove();
+//   })
+// //   add new functions here
 
-}
+// }
 
 
 
--- src/scratch.js
+++ src/scratch.js
@@ -1,29 +1,143 @@
-const form = document.getElementById('create-task-form');
-const uList = document.getElementById('tasks');
+// document.addEventListener('DOMContentLoaded', () => {
+//     addingEventListener()
+// })
+// let taskObjArr = []
 
-function addTaskToList(eventObj) {
-  eventObj.preventDefault();
-  const listItem = document.createElement('li');
-  const newText = eventObj.target['new-task-description'].value;
-  listItem.textContent = newText;
-  uList.append(listItem);
+// function addingEventListener() {
+//     document.getElementById("create-task-form")
+//     document.addEventListener("submit", handleFormSubmit)
+// };
 
-  const btn = document.createElement('button');
-  btn.textContent = "X";
-  listItem.append(btn);
+// function handleFormSubmit(e) {
 
-  btn.addEventListener('click', () => {
-    listItem.remove();
-  })
-}
+//     e.prevent.Default()
+//     // console.log(task)
+//     const task = e.target[0].value
+//     const priorityLevel = parseInt(e.target.priority.value)
 
-form.addEventListener('submit', addTaskToList);
+//     console.log(priorityLevel)
+    
 
+//     displayTask(task, priorityLevel)
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
+// }
 
-  
-})
+
+
+// function displayTask(task, priorityLevel) {
+//     const taskUl = document.getElementById("tasks")
+//     const taskLi = document.createElement("li")
+//     const deleteBtn = document.createElement("button")
+
+//     deleteBtn.textContent = "x"
+//     deleteBtn.addEventListener("click", deleteTask)
+
+
+
+//     task.textContent = task + " "
+//     // console.log(taskLi)
+//     task.li.style.color = getPriorityColor(priorityLevel)
+//     taskLi.appendChild(deleteBtn)
+//     taskUl.appendChild(taskLi)
+// }
+
+// function deleteTask(e){
+//     // console.log(e)
+//     e.target.parentNode.remove()
+
+// }
+// function getPriorityColor(priorityLevel) {
+//     // console.log(priorityLevel)
+//     if (priorityLevel === 1) {
+//         return "red"
+//     } else if (priorityLevel === 2) {
+//         return "cornflowerblue"
+//     } else {
+//         return "green"
+//     }
+
+// }
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    addingEventListeners()
+ })
+ 
+ let todos = []
+ 
+ function addingEventListeners() {
+    document
+       .getElementById("create-task-form")
+       .addEventListener("submit", handleFormSubmit)
+    document
+       .getElementById("sort")
+       .addEventListener("change", sortAndDisplayTodos)
+ }
+ 
+ function handleFormSubmit(e) {
+    e.preventDefault()
+ 
+    let todo = { task: e.target[0].value, priority: Number(e.target[1].value) }
+ 
+    addToArray(todo)
+    sortAndDisplayTodos()
+ 
+    e.target.reset()
+ }
+ 
+ function addToArray(todo) {
+    todos.push(todo)
+ }
+ 
+ function displayTodos() {
+    let tasks = document.getElementById("tasks")
+    tasks.innerHTML = ""
+ 
+    todos.forEach((todo) => {
+       let p = document.createElement("p")
+       let deleteBtn = document.createElement("button")
+ 
+       deleteBtn.textContent = "remove"
+       deleteBtn.addEventListener("click", (e) => {
+          deleteTask(e, todo)
+       })
+ 
+       p.textContent = `${todo.task} `
+ 
+       if (todo.priority === 1) {
+          p.style.color = "red"
+       } else if (todo.priority === 2) {
+          p.style.color = "green"
+       } else {
+          p.style.color = "blue"
+       }
+ 
+       p.appendChild(deleteBtn)
+ 
+       tasks.appendChild(p)
+    })
+ }
+ 
+ function deleteTask(e, todo) {
+    todos = todos.filter((el) => el.task !== todo.task)
+    e.target.parentNode.remove()
+ }
+ 
+ function sortAndDisplayTodos() {
+    let sort = document.getElementById("sort")
+ 
+    if (sort.value === "high-low") {
+       todos.sort((a, b) => a.priority - b.priority)
+    } else {
+       todos.sort((a, b) => b.priority - a.priority)
+    }
+ 
+    displayTodos()
+ }
 
